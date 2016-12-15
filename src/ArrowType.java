e1374d488d042bbcdf4214aa41ebe0b85010007d
weissna <weissna@rose-hulman.edu>
2016-12-15 18:49:44 -0500

    Add files via upload

---
+++ src/ArrowType.java
@@ -0,0 +1,17 @@
+package SP;
+
+public class ArrowType {
+
+	public static int FULL_HEAD=0;
+	public static int EMPTY_HEAD=1;
+	
+	public static int FULL_BODY=7;
+	
+	public static ArrowType EXTENDS_ARROW;
+	
+	private int red;
+	private int green;
+	private int blue;
+	private int head;
+	private int body;
+}
---
+++ src/CodeReader.java
@@ -0,0 +1,22 @@
+package SP;
+
+import java.util.List;
+
+public class CodeReader {
+
+	public ClassNode getClass(String name){
+		return null;
+	}
+	
+	public ClassNode getSuper(ClassNode node){
+		return null;
+	}
+	
+	public List<ClassNode> getInterfaces(ClassNode node){
+		return null;
+	}
+	
+	public List<ClassNode> getFields(ClassNode node){
+		return null;
+	}
+}
